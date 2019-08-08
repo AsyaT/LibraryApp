d2928f69c8b3ff3f9163587094bd6aa745e4c65a
Asya Troyan <asya99@yandex.ru>
2019-08-08 18:39:04 +0500

    remove useless code

--- LibraryApp/Controllers/HomeController.cs
+++ LibraryApp/Controllers/HomeController.cs
@@ -42,18 +42,5 @@ namespace LibraryApp.Controllers
             return View(books);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+   }
 }
