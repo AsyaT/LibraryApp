b1f270c468ae56eb9120854c6250b586cfac9901
Asya Troyan <asya99@yandex.ru>
2019-08-08 18:28:12 +0500

    Fix list of books

--- LibraryApp/Controllers/HomeController.cs
+++ LibraryApp/Controllers/HomeController.cs
@@ -11,17 +11,34 @@ namespace LibraryApp.Controllers
     {
         public ActionResult Index()
         {
+            List<BookModel> books = new List<BookModel>();
+
             List<AuthorModel> tolstoy = new List<AuthorModel>();
-            tolstoy.Add(new AuthorModel() { FirstName = "Lev", LastName = "Tolstoy" });
+            tolstoy.Add(new AuthorModel() { FirstName = "Лев", LastName = "Толстой" });
 
 
-            BookModel books = new BookModel() {
+            BookModel warAndPeace = new BookModel() {
             Title="Война и мир",
             Authors = tolstoy,
             YearOfPublication=new DateTime(2014,1,1),
             Publisher= "Азбука , Азбука-Аттикус",
             NumberOfPages = 660
             };
+
+            List<AuthorModel> strugatskie = new List<AuthorModel>();
+            strugatskie.Add(new AuthorModel() { FirstName = "Аркадий", LastName = "Стругацкий" });
+            strugatskie.Add(new AuthorModel() { FirstName = "Борис", LastName = "Стругацкий" });
+
+            BookModel piknik = new BookModel() {
+            Title = "Пикник на обочине",
+            Authors = strugatskie,
+            YearOfPublication= new DateTime(2017,1,1),
+            Publisher= "Neoclassic, АСТ",
+            NumberOfPages=240};
+
+            books.Add(warAndPeace);
+            books.Add(piknik);
+
             return View(books);
         }
 
