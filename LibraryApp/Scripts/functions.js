ea2fb8d4e55f2bd7b69cdbb8e682a9a46ed2a1b7
Asya Troyan <asya99@yandex.ru>
2019-08-09 16:13:37 +0500

    update table

--- LibraryApp/Scripts/functions.js
+++ LibraryApp/Scripts/functions.js
@@ -1,20 +1,9 @@
 ﻿
     function replaceTable() {
-        alert(1);
-        
-            //Send the AJAX call to the server
             $.ajax({
-                //The URL to process the request
                 'url': '/Home/BooksTable',
-                //The type of request, also known as the "method" in HTML forms
-                //Can be 'GET' or 'POST'
                 'type': 'GET',
-                //Any post-data/get-data parameters
-                //This is optional
-                
-                //The response from the server
                 'success': function (data) {
-                    //You can use any jQuery/JavaScript here!!!
                     $('#books-table').html(data);
                 }
             
