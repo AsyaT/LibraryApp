722c586c59a653b0a2b6733eb84a789d92a197cb
Asya Troyan <asya99@yandex.ru>
2019-08-11 01:01:13 +0500

    names for forms

--- LibraryApp/Models/AuthorModel.cs
+++ LibraryApp/Models/AuthorModel.cs
@@ -4,9 +4,11 @@ namespace LibraryApp.Models
 {
     public class AuthorModel
     {
+        [Display(Name = "First name")]
         [StringLength(20, ErrorMessage = "First name cannot be longer than 20 characters.")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last name")]
         [StringLength(20, ErrorMessage = "Last name cannot be longer than 20 characters.")]
         public string LastName { get; set; }
     }
--- LibraryApp/Models/BookModel.cs
+++ LibraryApp/Models/BookModel.cs
@@ -17,24 +17,30 @@ namespace LibraryApp.Models
 
         [Required]
         [StringLength(30, ErrorMessage = "Title cannot be longer than 30 characters.")]
+        [Display(Name = "Title")]
         public string Title { get; set; }
 
         [NotEmptyCollection]
         [NameisValid]
+        [Display(Name = "Authors")]
         public IList<AuthorModel> Authors { get; set; }
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "Number of pages")]
         public uint NumberOfPages { get; set; }
 
         [StringLength(30, ErrorMessage = "Publisher name cannot be longer than 20 characters.")]
+        [Display(Name = "Name of publisher")]
         public string Publisher { get; set; }
 
         [Required]
         [YearMinimumRange(1800)]
+        [Display(Name = "Year of publication")]
         public int YearOfPublication { get; set; }
 
         [DataType(DataType.Upload)]
+        [Display(Name = "Image")]
         public HttpPostedFileBase Image { get; set; } 
     }
 }
