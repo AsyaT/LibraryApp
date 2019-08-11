dd6d439737554ca48be3c3d756baf279b8ce5f5a
Asya Troyan <asya99@yandex.ru>
2019-08-11 12:08:35 +0500

    fix removing of row

--- LibraryApp/Scripts/functions.js
+++ LibraryApp/Scripts/functions.js
@@ -9,20 +9,3 @@
             
         });
     }
-
-    $("a.btn-remove-author-details").click(function (event) {
-        event.preventDefault();
-        console.log($(this).context.outerHTML);
-
-        $(this).parents('.author-details').remove();
-    });
-
-    //function removeAuthorDetails(event)
-    //{
-    //    //alert($(this).parent('.author-details'));
-    //    //event.preventDefault();
-    //    console.log($(this).context.outerHTML);
-    //    console.log($(this).parents('.author-details').context.outerHTML);
-    //    $(this).parents('.author-details').remove();
-        
-    //}
