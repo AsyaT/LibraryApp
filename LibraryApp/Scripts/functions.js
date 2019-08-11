61e8caaa818ae7a4f88bae7ef512bf44290f33fc
Asya Troyan <asya99@yandex.ru>
2019-08-11 11:16:05 +0500

    not finished - jquery remove button

--- LibraryApp/Scripts/functions.js
+++ LibraryApp/Scripts/functions.js
@@ -10,3 +10,19 @@
         });
     }
 
+    $("a.btn-remove-author-details").click(function (event) {
+        event.preventDefault();
+        console.log($(this).context.outerHTML);
+
+        $(this).parents('.author-details').remove();
+    });
+
+    //function removeAuthorDetails(event)
+    //{
+    //    //alert($(this).parent('.author-details'));
+    //    //event.preventDefault();
+    //    console.log($(this).context.outerHTML);
+    //    console.log($(this).parents('.author-details').context.outerHTML);
+    //    $(this).parents('.author-details').remove();
+        
+    //}
